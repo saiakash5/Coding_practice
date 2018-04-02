354e594de4c2f483525e4f6adf0faf3e75573d14
Akash <saiakash5@gmail.com>
2018-04-02 11:22:05 -0700

    added factorial to Recursion examples

--- src/Recursion_Examples/Recursion_based_questions.java
+++ src/Recursion_Examples/Recursion_based_questions.java
@@ -4,4 +4,24 @@ package Recursion_Examples;
  * Created by Akash on 4/2/18.
  */
 public class Recursion_based_questions {
+    public static void main(String[] args)
+    {
+        Recursion r_obj = new Recursion();
+        System.out.println(r_obj.factorial(5));
+    }
+}
+
+class  Recursion
+{
+    public int factorial(int n)
+    {
+        if (n ==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * factorial(n-1);
+        }
+    }
 }
